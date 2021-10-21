d82f50d92af5cb10b398220bc77915ecba1d6dfd
sergicasado <sergicasado1@iesebre.com>
2021-10-21 19:03:10 +0200

    contador fet

--- app/src/main/java/me/sergicasadopellissa/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/sergicasadopellissa/dam/comptador/MainActivity.kt
@@ -2,10 +2,43 @@ package me.sergicasadopellissa.dam.comptador
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var timeTextView: TextView
+    internal lateinit var counterTextView: TextView
+    internal var counter = 0
+    internal var time = 60
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tapMeButton = findViewById(R.id.tapMeButton)
+        timeTextView = findViewById(R.id.timeTextView)
+        counterTextView = findViewById(R.id.counterTextView)
+
+        //Actualitzar o definir valor inicial de counterTextView -> counterTextView = score -> 0
+
+        //TODO en algun moment haurem d'executar incrementCounter
+
+        tapMeButton.setOnClickListener(){
+            incrementCounter()
+        }
+
+
+        //timeTextView.text = time.toString()
+        timeTextView.text = getString(R.string.timeText, time)
+
+    }
+
+    private fun incrementCounter() {
+        //      counter = counter +1
+        counter += 1
+        counterTextView.text = counter.toString()
+
     }
 }
